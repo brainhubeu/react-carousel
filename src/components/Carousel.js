6add4061dbaf4d5d726810eaf75fa5d72d32e29b
Bartłomiej Koppel <bartek@brainhub.pl>
2018-01-03 11:30:03 +0100

    add first css and html

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -16,10 +16,23 @@ export default class Carousel extends Component {
     ));
   }
 
+  renderCarouselDots() {
+    return this.props.carouselItems.map((carouselItem, index) => (
+      <li><button>{index + 1}</button></li>
+    ));
+  }
+
   render() {
     return (
-      <div className={classnames('brainhub-carousel-items-container', this.props.additionalContainerClassName)}>
-        {this.renderCarouselItems()}
+      <div className={classnames('brainhub-carousel', this.props.additionalContainerClassName)}>
+        <button className="brainhub-carousel__arrows brainhub-carousel__arrow-left" type="button">Prev</button>
+        <ul className="brainhub-carousel__track">
+          {this.renderCarouselItems()}
+        </ul>
+        <button className="brainhub-carousel__arrows brainhub-carousel__arrow-right" type="button">Next</button>
+        <ul className="brainhub-carousel__dots">
+          {this.renderCarouselDots()}
+        </ul>
       </div>
     );
   }
