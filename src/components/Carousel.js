484112c41cf5f98ea79737ff2f413c85f070e422
shelooks16 <blauyedz@zoho.eu>
2019-11-28 03:14:38 +0100

    attach listeners to parent element

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -90,11 +90,11 @@ export default class Carousel extends Component {
 
     // adding event listeners for swipe
     if (this.node) {
-      this.node.ownerDocument.addEventListener('mousemove', this.onMouseMove, true);
-      this.node.ownerDocument.addEventListener('mouseup', this.onMouseUpTouchEnd, true);
-      this.node.ownerDocument.addEventListener('touchstart', this.simulateEvent, true);
-      this.node.ownerDocument.addEventListener('touchmove', this.simulateEvent, { passive: false });
-      this.node.ownerDocument.addEventListener('touchend', this.simulateEvent, true);
+      this.node.parentElement.addEventListener('mousemove', this.onMouseMove, true);
+      this.node.parentElement.addEventListener('mouseup', this.onMouseUpTouchEnd, true);
+      this.node.parentElement.addEventListener('touchstart', this.simulateEvent, true);
+      this.node.parentElement.addEventListener('touchmove', this.simulateEvent, { passive: false });
+      this.node.parentElement.addEventListener('touchend', this.simulateEvent, true);
     }
 
     // setting size of a carousel in state
@@ -133,11 +133,11 @@ export default class Carousel extends Component {
     this.trackRef && this.trackRef.removeEventListener('transitionend', this.onTransitionEnd);
 
     if (this.node) {
-      this.node.ownerDocument.removeEventListener('mousemove', this.onMouseMove);
-      this.node.ownerDocument.removeEventListener('mouseup', this.onMouseUp);
-      this.node.ownerDocument.removeEventListener('touchstart', this.simulateEvent);
-      this.node.ownerDocument.removeEventListener('touchmove', this.simulateEvent);
-      this.node.ownerDocument.removeEventListener('touchend', this.simulateEvent);
+      this.node.parentElement.removeEventListener('mousemove', this.onMouseMove);
+      this.node.parentElement.removeEventListener('mouseup', this.onMouseUp);
+      this.node.parentElement.removeEventListener('touchstart', this.simulateEvent);
+      this.node.parentElement.removeEventListener('touchmove', this.simulateEvent);
+      this.node.parentElement.removeEventListener('touchend', this.simulateEvent);
     }
 
     window.removeEventListener('resize', this.onResize);
