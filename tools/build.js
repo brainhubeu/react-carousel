d3aacfb8fe1bb349df8cc126ef7edbf318ce9102
Piotr Sroczkowski <piotr.sroczkowski@brainhub.pl>
2020-01-28 20:33:47 +0100

    [198] tools/build.js from master

--- tools/build.js
+++ tools/build.js
@@ -2,7 +2,9 @@
 // Allowing console calls below since this is a build file.
 /* eslint-disable no-console */
 const webpack = require('webpack');
+
 const config = require('../webpack.config.prod');
+
 const { chalkError, chalkSuccess, chalkWarning, chalkProcessing } = require('./chalkConfig');
 
 process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
