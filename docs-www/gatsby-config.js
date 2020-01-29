028acd972c548ad85df4e39198a1b6b0ce738362
Piotr Sroczkowski <piotr.sroczkowski@brainhub.pl>
2020-01-29 02:00:25 +0100

    [198] custom build

--- docs-www/gatsby-config.js
+++ docs-www/gatsby-config.js
@@ -5,13 +5,13 @@ const pluginConfigFactory = require('@brainhubeu/gatsby-docs-kit/plugins');
 
 module.exports = {
   siteMetadata: {
-    title: 'React-carousel',
+    title: 'React-carousel #202',
     description: 'Feature-rich, react-way react component that does not suck',
     image: 'https://cdn-images-1.medium.com/max/1200/1*CLUFZFaXF6NG27NA3d_JkQ.jpeg',
-    url: 'https://brainhubeu.github.io/react-carousel',
+    url: 'https://github.com/brainhubeu/react-carousel/pull/202',
     type: 'article',
     siteName: 'React-carousel',
-    githubUrl: 'https://github.com/brainhubeu/react-carousel/',
+    githubUrl: 'https://github.com/brainhubeu/react-carousel/pull/202',
   },
 
   // URL prefix on production environment. For more info see https://www.gatsbyjs.org/docs/path-prefix/
