2f08e23a2651c758ff6281160435fd99831078fb
Piotr Sroczkowski <piotr.sroczkowski@brainhub.pl>
2020-01-31 04:44:02 +0100

    [198] true

--- docs-www/src/globalReferences.js
+++ docs-www/src/globalReferences.js
@@ -1,7 +1,7 @@
 console.log({ env: process.env });
 
 const { default: Carousel, Dots } = (() => {
-  if ('__RC_ENV__' === 'development') {
+  if (true) {
     console.log('connecting with local react-carousel source code');
     return require('../../src');
   } else {
