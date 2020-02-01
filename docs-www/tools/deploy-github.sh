58d9cd5fef59555deddaff71ee9c8273af7bfeb4
Piotr Sroczkowski <piotr.sroczkowski@brainhub.pl>
2020-02-01 22:45:35 +0100

    remove gh-pages

--- docs-www/tools/deploy-github.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GITHUB_REPO_NAME=$(basename -s .git `git config --get remote.origin.url`);
-
-PATH_PREFIX="/$GITHUB_REPO_NAME" npm run build
-
-# deploy to github pags
-node ./node_modules/.bin/gh-pages -d public
