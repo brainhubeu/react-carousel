3b320914e79b02d26ac7ac79b8a8fd8831d7a945
Piotr Sroczkowski <piotr.sroczkowski@brainhub.pl>
2020-02-14 03:28:02 +0100

    [152] @babel/register and @babel/polyfill fix

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -17,8 +17,8 @@ global.logger.warn = function() {};
 
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
-require('babel-register')();
-require('babel-polyfill');
+require('@babel/register')();
+require('@babel/polyfill');
 
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
