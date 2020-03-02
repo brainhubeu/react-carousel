93e3b4465df283a0b4cdbf58c0566b35a6cec3b0
Robert Hebel <robert@brainhub.pl>
2020-03-02 17:35:02 +0100

    Move first test to Hermione

---
+++ .hermione.conf.js
@@ -0,0 +1,32 @@
+module.exports = {
+  sets: {
+    desktop: {
+      files: 'test/regression'
+    }
+  },
+
+  baseUrl: 'http://localhost:8000',
+  gridUrl: 'http://localhost:4444/wd/hub',
+  compositeImage: true,
+
+  browsers: {
+    chromeXL: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          // args: ['--headless'],
+        },
+      },
+      windowSize: '1300x900',
+    },
+  },
+
+  plugins: {
+    'html-reporter/hermione': {
+      enabled: true,
+      path: 'my/hermione-reports',
+      defaultView: 'all',
+      baseHost: 'http://localhost:3000'
+    }
+  }
+};
