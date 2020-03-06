9a7f235946981542910541fa6ff49a622a4d615a
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-03-06 02:10:31 +0100

    [305] provide String.prototype.endsWith

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import Carousel from './components/CarouselWrapper';
 import CarouselDots from './components/CarouselDots';
 
+if (!String.prototype.endsWith) {
+  // eslint-disable-next-line no-extend-native
+  String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+  };
+}
+
 export const Dots = CarouselDots;
 export default Carousel;
