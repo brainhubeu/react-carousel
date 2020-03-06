18eba6226e00841f4ff9463bcb1698f1ddb1086b
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-03-06 02:40:56 +0100

    [305] change order

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import Carousel from './components/CarouselWrapper';
-import CarouselDots from './components/CarouselDots';
-
+/* eslint-disable import/first */
 if (!String.prototype.endsWith) {
   // eslint-disable-next-line no-extend-native
   String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
   };
 }
+
+import Carousel from './components/CarouselWrapper';
+import CarouselDots from './components/CarouselDots';
 
 export const Dots = CarouselDots;
 export default Carousel;
