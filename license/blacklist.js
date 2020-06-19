467cf52909c30152bb74e91ba3847cc91e32f93e
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-06-20 01:13:07 +0200

    add to whitelist

--- license/blacklist.js
+++ license/blacklist.js
@@ -1,6 +1,5 @@
 /* Provide a list of blacklisted licenses for the project below */
 const blacklist = [
-  'MIT',
 ];
 
 module.exports = blacklist;
--- license/whitelist.js
+++ license/whitelist.js
@@ -1,6 +1,12 @@
 /* Provide a list of whitelisted licenses for the project below */
 const whitelist = [
-
+  'Apache-2.0',
+  'BSD-2-Clause',
+  'BSD-3-Clause',
+  'ISC',
+  'MIT',
+  'WTFPL',
+  '0BSD',
 ];
 
 module.exports = whitelist;
