0957389599407419cf1495db22879b0ffbbbd980
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-06-20 14:05:25 +0200

    add others to whitelist and sort

--- license/whitelist.js
+++ license/whitelist.js
@@ -1,17 +1,23 @@
 /* Provide a list of whitelisted licenses for the project below */
 const whitelist = [
+  '(MIT AND BSD-3-Clause)',
+  '(MIT AND CC-BY-3.0)',
+  '(MIT AND Zlib)',
+  '(MIT OR Apache-2.0)',
+  '(MIT OR CC0-1.0)',
+  '(WTFPL OR MIT)',
+  '0BSD',
   'Apache-2.0',
   'BSD-2-Clause',
+  'BSD-3-Clause OR MIT',
   'BSD-3-Clause',
+  'CC-BY-3.0',
+  'CC-BY-4.0',
+  'CC0-1.0',
   'ISC',
   'MIT',
   'WTFPL',
-  '0BSD',
-  'CC-BY-4.0',
-  'CC0-1.0',
-  '(MIT OR CC0-1.0)',
-  'CC-BY-3.0',
-  '(MIT AND CC-BY-3.0)',
+  'Zlib',
 ];
 
 module.exports = whitelist;
