36193323aa82113e3238e0d2e4dfeab88d25292f
roberthebel <robert@brainhub.pl>
2020-07-09 13:31:33 +0200

    Use local carousel in docs until the v2 will be released

--- docs-www/src/globalReferences.js
+++ docs-www/src/globalReferences.js
@@ -24,16 +24,8 @@ const {
   if (!global.window) {
     global.window = {};
   }
-  if (process.env.NODE_ENV === 'development') {
-    console.log('connecting with local react-carousel source code');
-    return require('../../react-carousel/src');
-  } else {
-    console.log(
-      'connecting with @brainhubeu/react-carousel installed in node_modules',
-    );
-    require('@brainhubeu/react-carousel/lib/style.css');
-    return require('@brainhubeu/react-carousel');
-  }
+  console.log('connecting with local react-carousel source code');
+  return require('../../react-carousel/src');
 })();
 
 export {
