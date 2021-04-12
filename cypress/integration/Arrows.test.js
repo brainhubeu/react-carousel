8c0728e0e516fbd0bd59c3551ba1af5d5d6caa4e
RobertHebel <robert.hebel1@outlook.com>
2021-04-12 21:43:01 +0200

    Fix production builds using webpack (#646) (#688)
    * Fix production builds using webpack (#646)
    
    * fix: Only use peerDependencies for react and react-dom
    
    * fix: Correctly set production mode in webpack & bump recoil version
    
    * style: fix prettier errors
    
    * chore: bump recoil version
    
    * test: fix arrows e2e tests
    
    Co-authored-by: mnelemans <mnelemans@users.noreply.github.com>

--- cypress/integration/Arrows.test.js
+++ cypress/integration/Arrows.test.js
@@ -11,12 +11,16 @@ describe('Arrows', () => {
   it('changes slide when clicks on the arrow', () => {
     cy.get('.BrainhubCarousel__arrowRight').trigger('click');
 
+    cy.wait(1);
+
     cy.get('.BrainhubCarouselItem--active')
       .children('img')
       .should('have.attr', 'src')
       .and('contain', 'scream');
 
     cy.get('.BrainhubCarousel__arrowLeft').trigger('click');
+
+    cy.wait(1);
 
     cy.get('.BrainhubCarouselItem--active')
       .children('img')
@@ -27,6 +31,7 @@ describe('Arrows', () => {
   it(`doesn't allow to click on the arrow if there is no more slides`, () => {
     for (let i = 0; i < 2; i++) {
       cy.get('.BrainhubCarousel__arrowRight').trigger('click');
+      cy.wait(1);
     }
 
     cy.get('.BrainhubCarouselItem--active')
